9e7016dba3ac5f1229c93d4049a21496fa4e1f47
Anthony Salas <anthony.salas@tecsup.edu.pe>
2022-10-30 20:57:45 -0500

    create02

--- api/users/views.py
+++ api/users/views.py
@@ -17,7 +17,7 @@ class JSONResponse(HttpResponse):
         super(JSONResponse, self).__init__(content, **kwargs)
 
 
-@csrf_exempt
+
 def user_list(request):
     """
     List all code serie, or create a new serie.
@@ -35,7 +35,6 @@ def user_list(request):
             return JSONResponse(serializer.data, status=201)
         return JSONResponse(serializer.errors, status=400)
 
-@csrf_exempt
 def user_detail(request, pk):
     """
     Retrieve, update or delete a serie.
@@ -62,7 +61,6 @@ def user_detail(request, pk):
         return HttpResponse(status=204)
 
 
-@csrf_exempt
 def unidad_list(request):
     """
     List all code serie, or create a new serie.
@@ -81,7 +79,7 @@ def unidad_list(request):
         return JSONResponse(serializer.errors, status=400)
 
 
-@csrf_exempt
+
 def unidad_detail(request, pk):
     """
     Retrieve, update or delete a serie.
